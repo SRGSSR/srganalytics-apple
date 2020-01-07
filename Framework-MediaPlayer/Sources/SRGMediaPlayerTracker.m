30c939b48ae8d4e1a00778c190d5ae394aa761c4
Samuel Défago <defagos@gmail.com>
2020-01-07 08:44:51 +0100

    Update dependencies

--- Framework-MediaPlayer/Sources/SRGMediaPlayerTracker.m
+++ Framework-MediaPlayer/Sources/SRGMediaPlayerTracker.m
@@ -186,7 +186,8 @@ static NSMutableDictionary<NSValue *, SRGMediaPlayerTracker *> *s_trackers = nil
         
         self.lastEvent = event;
         
-        // Restore the heartbeat timer when transitioning to play again.
+        // Restore the heartbeat timer when transitioning to play again. We can use a simple `NSTimer` here since
+        // it needs to run while playing content (even in background), but will otherwise be inactive.
         if ([event isEqualToString:MediaPlayerTrackerEventPlay]) {
             if (! self.heartbeatTimer) {
                 SRGAnalyticsConfiguration *configuration = SRGAnalyticsTracker.sharedTracker.configuration;
