d086a3aa404189c9a9afeabb96d0b444f932e330
Pierre-Yves Bertholon <py.bertholon@gmail.com>
2022-06-13 18:58:34 +0200

    Update srgssr stream urls

--- Tests/SRGAnalyticsTests/ComScoreMediaPlayerTestCase.m
+++ Tests/SRGAnalyticsTests/ComScoreMediaPlayerTestCase.m
@@ -18,12 +18,12 @@ static NSURL *OnDemandTestURL(void)
 
 static NSURL *LiveTestURL(void)
 {
-    return [NSURL URLWithString:@"https://rtsc3video-lh.akamaihd.net/i/rtsc3video_ww@513975/master.m3u8?dw=0"];
+    return [NSURL URLWithString:@"https://rtsc3video.akamaized.net/hls/live/2042837/c3video/3/playlist.m3u8?dw=0"];
 }
 
 static NSURL *DVRTestURL(void)
 {
-    return [NSURL URLWithString:@"https://rtsc3video-lh.akamaihd.net/i/rtsc3video_ww@513975/master.m3u8"];
+    return [NSURL URLWithString:@"https://rtsc3video.akamaized.net/hls/live/2042837/c3video/3/playlist.m3u8"];
 }
 
 @interface ComScoreMediaPlayerTestCase : XCTestCase
@@ -586,8 +586,8 @@ static NSURL *DVRTestURL(void)
 {
     [self expectationForComScorePlayerEventNotificationWithHandler:^BOOL(NSString *event, NSDictionary *labels) {
         XCTAssertEqualObjects(labels[@"ns_st_ev"], @"play");
-        XCTAssertEqualObjects(labels[@"ns_st_ldo"], @"0");
-        XCTAssertEqualObjects(labels[@"ns_st_ldw"], @"21550000");
+        XCTAssertEqualObjects(labels[@"ns_st_ldo"], @"1000");
+        XCTAssertEqualObjects(labels[@"ns_st_ldw"], @"21552000");
         return YES;
     }];
     
@@ -603,15 +603,15 @@ static NSURL *DVRTestURL(void)
             XCTAssertFalse(pauseReceived);
             XCTAssertFalse(playReceived);
             
-            XCTAssertEqualObjects(labels[@"ns_st_ldo"], @"0");
-            XCTAssertEqualObjects(labels[@"ns_st_ldw"], @"21550000");
+            XCTAssertEqualObjects(labels[@"ns_st_ldo"], @"1000");
+            XCTAssertEqualObjects(labels[@"ns_st_ldw"], @"21552000");
             pauseReceived = YES;
         }
         else if ([labels[@"ns_st_ev"] isEqualToString:@"play"]) {
             XCTAssertFalse(playReceived);
             
             XCTAssertEqualObjects(labels[@"ns_st_ldo"], @"45000");
-            XCTAssertEqualObjects(labels[@"ns_st_ldw"], @"21550000");
+            XCTAssertEqualObjects(labels[@"ns_st_ldw"], @"21552000");
             playReceived = YES;
         }
         return pauseReceived && playReceived;
@@ -625,7 +625,7 @@ static NSURL *DVRTestURL(void)
     [self expectationForComScorePlayerEventNotificationWithHandler:^BOOL(NSString *event, NSDictionary *labels) {
         XCTAssertEqualObjects(labels[@"ns_st_ev"], @"end");
         XCTAssertEqualObjects(labels[@"ns_st_ldo"], @"45000");
-        XCTAssertEqualObjects(labels[@"ns_st_ldw"], @"21550000");
+        XCTAssertEqualObjects(labels[@"ns_st_ldw"], @"21552000");
         return YES;
     }];
     
--- Tests/SRGAnalyticsTests/MediaPlayerTestCase.m
+++ Tests/SRGAnalyticsTests/MediaPlayerTestCase.m
@@ -33,12 +33,12 @@ static NSURL *OnDemandAudioTestURL(void)
 
 static NSURL *LiveTestURL(void)
 {
-    return [NSURL URLWithString:@"https://rtsc3video-lh.akamaihd.net/i/rtsc3video_ww@513975/master.m3u8?dw=0"];
+    return [NSURL URLWithString:@"https://rtsc3video.akamaized.net/hls/live/2042837/c3video/3/playlist.m3u8?dw=0"];
 }
 
 static NSURL *DVRTestURL(void)
 {
-    return [NSURL URLWithString:@"https://rtsc3video-lh.akamaihd.net/i/rtsc3video_ww@513975/master.m3u8"];
+    return [NSURL URLWithString:@"https://rtsc3video.akamaized.net/hls/live/2042837/c3video/3/playlist.m3u8"];
 }
 
 @interface MediaPlayerTestCase : XCTestCase
