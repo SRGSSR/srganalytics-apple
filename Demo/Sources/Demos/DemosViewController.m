47f32ef34beb8c864254716b019561c57644ec19
Pierre-Yves Bertholon <py.bertholon@gmail.com>
2022-11-29 21:39:57 +0100

    Fix demo warning

--- Demo/Sources/Demos/DemosViewController.m
+++ Demo/Sources/Demos/DemosViewController.m
@@ -235,7 +235,9 @@ static NSString * const LastLoggedInEmailAddress = @"LastLoggedInEmailAddress";
             
             SRGMediaPlayerViewController *playerViewController = [[SRGMediaPlayerViewController alloc] init];
             playerViewController.modalPresentationStyle = UIModalPresentationFullScreen;
-            playerViewController.allowsPictureInPicturePlayback = NO;
+            if (@available(tvOS 14.0, *)) {
+                playerViewController.allowsPictureInPicturePlayback = NO;
+            }
             [playerViewController.controller playURL:URL atPosition:nil withSegments:nil analyticsLabels:labels userInfo:nil];
             [self presentViewController:playerViewController animated:YES completion:nil];
             break;
