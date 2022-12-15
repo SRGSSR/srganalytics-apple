71f2d082f9b4a980d72d470303a4e9fbeb2c841c
Pierre-Yves Bertholon <py.bertholon@gmail.com>
2022-12-15 16:11:16 +0100

    Add default labels for TCPageViewEvent and TCCustomEvent

--- Sources/SRGAnalytics/SRGAnalyticsTracker.m
+++ Sources/SRGAnalytics/SRGAnalyticsTracker.m
@@ -48,6 +48,9 @@ void SRGAnalyticsRenewUnitTestingIdentifier(void)
 
 @property (nonatomic) SRGAnalyticsLabels *globalLabels;
 
+@property (nonatomic, readonly) NSDictionary *defaultComScoreLabels;
+@property (nonatomic, readonly) NSDictionary *defaultLabels;
+
 @end
 
 @implementation SRGAnalyticsTracker
@@ -297,7 +300,7 @@ void SRGAnalyticsRenewUnitTestingIdentifier(void)
 {
     NSAssert(title.length != 0, @"A title is required");
     
-    NSMutableDictionary<NSString *, NSString *> *fullLabels = [self defaultComScoreLabels].mutableCopy;
+    NSMutableDictionary<NSString *, NSString *> *fullLabels = self.defaultComScoreLabels.mutableCopy;
     [fullLabels srg_safelySetString:title forKey:@"srg_title"];
     [fullLabels srg_safelySetString:@(fromPushNotification).stringValue forKey:@"srg_ap_push"];
     
@@ -345,7 +348,7 @@ void SRGAnalyticsRenewUnitTestingIdentifier(void)
                                      labels:(SRGAnalyticsPageViewLabels *)labels
                        fromPushNotification:(BOOL)fromPushNotification
 {
-    NSMutableDictionary<NSString *, NSString *> *fullLabels = [NSMutableDictionary dictionary];
+    NSMutableDictionary<NSString *, NSString *> *fullLabels = self.defaultLabels.mutableCopy;
     [fullLabels srg_safelySetString:@"app" forKey:@"navigation_property_type"];
     [fullLabels srg_safelySetString:self.configuration.businessUnitIdentifier.uppercaseString forKey:@"navigation_bu_distributer"];
     [fullLabels srg_safelySetString:fromPushNotification ? @"true" : @"false" forKey:@"accessed_after_push_notification"];
@@ -399,7 +402,7 @@ void SRGAnalyticsRenewUnitTestingIdentifier(void)
 {
     NSAssert(self.configuration != nil, @"The tracker must be started");
 
-    NSMutableDictionary<NSString *, NSString *> *fullLabels = [NSMutableDictionary dictionary];
+    NSMutableDictionary<NSString *, NSString *> *fullLabels = self.defaultLabels.mutableCopy;
     [fullLabels srg_safelySetString:name forKey:@"event_name"];
 
     NSDictionary<NSString *, NSString *> *labelsDictionary = [labels labelsDictionary];
