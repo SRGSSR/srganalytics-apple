f288c4ea2b3beffb7e8165eb239543ec9fd5d0d3
Pierre-Yves Bertholon <py.bertholon@gmail.com>
2023-07-08 15:42:34 +0200

    Add page view tests

--- Tests/SRGAnalyticsTests/TrackerTestCase.m
+++ Tests/SRGAnalyticsTests/TrackerTestCase.m
@@ -94,4 +94,105 @@
     }];
 }
 
+- (void)testPageViewEvent
+{
+    [self expectationForPageViewEventNotificationWithHandler:^BOOL(NSString * event, NSDictionary * labels) {
+        XCTAssertEqualObjects(event, @"page_view"); // Commanders Act SDK property
+        XCTAssertEqualObjects(labels[@"page_type"], @"Page view"); // Commanders Act SDK property
+        XCTAssertNotNil(labels[@"accessed_after_push_notification"]);
+        XCTAssertFalse([labels[@"accessed_after_push_notification"] boolValue]);
+        XCTAssertEqualObjects(labels[@"navigation_property_type"], @"app");
+        XCTAssertEqualObjects(labels[@"navigation_bu_distributer"], @"RTS");
+        return YES;
+    }];
+    
+    [SRGAnalyticsTracker.sharedTracker trackPageViewWithTitle:@"Page view" levels:nil];
+    
+    [self waitForExpectationsWithTimeout:20. handler:nil];
+}
+
+- (void)testPageViewEventWithLevels
+{
+    [self expectationForPageViewEventNotificationWithHandler:^BOOL(NSString * event, NSDictionary * labels) {
+        XCTAssertEqualObjects(event, @"page_view"); // Commanders Act SDK property
+        XCTAssertEqualObjects(labels[@"page_type"], @"Page view"); // Commanders Act SDK property
+        XCTAssertEqualObjects(labels[@"navigation_level_1"], @"level 1");
+        XCTAssertEqualObjects(labels[@"navigation_level_2"], @"level 2");
+        XCTAssertEqualObjects(labels[@"navigation_level_3"], @"level 3");
+        XCTAssertEqualObjects(labels[@"navigation_level_4"], @"level 4");
+        XCTAssertEqualObjects(labels[@"navigation_level_5"], @"level 5");
+        XCTAssertEqualObjects(labels[@"navigation_level_6"], @"level 6");
+        XCTAssertEqualObjects(labels[@"navigation_level_7"], @"level 7");
+        XCTAssertEqualObjects(labels[@"navigation_level_8"], @"level 8");
+        XCTAssertNil(labels[@"navigation_level_9"]);
+        return YES;
+    }];
+    
+    NSArray<NSString *> *levels = @[ @"level 1", @"level 2", @"level 3", @"level 4", @"level 5", @"level 6", @"level 7", @"level 8", @"level 9" ];
+    
+    [SRGAnalyticsTracker.sharedTracker trackPageViewWithTitle:@"Page view" levels:levels];
+    
+    [self waitForExpectationsWithTimeout:20. handler:nil];
+}
+
+- (void)testPageViewEventWithLabels
+{
+    [self expectationForPageViewEventNotificationWithHandler:^BOOL(NSString * event, NSDictionary * labels) {
+        XCTAssertEqualObjects(event, @"page_view"); // Commanders Act SDK property
+        XCTAssertEqualObjects(labels[@"page_type"], @"Page view"); // Commanders Act SDK property
+        XCTAssertEqualObjects(labels[@"custom_label"], @"custom_value");
+        return YES;
+    }];
+    
+    SRGAnalyticsPageViewLabels *labels = [[SRGAnalyticsPageViewLabels alloc] init];
+    labels.customInfo = @{ @"custom_label" : @"custom_value" };
+    
+    [SRGAnalyticsTracker.sharedTracker trackPageViewWithTitle:@"Page view" levels:nil labels:labels fromPushNotification:NO];
+    
+    [self waitForExpectationsWithTimeout:20. handler:nil];
+}
+
+- (void)testPageViewEventWithLevelsAndLabels
+{
+    [self expectationForPageViewEventNotificationWithHandler:^BOOL(NSString * event, NSDictionary * labels) {
+        XCTAssertEqualObjects(event, @"page_view"); // Commanders Act SDK property
+        XCTAssertEqualObjects(labels[@"page_type"], @"Page view"); // Commanders Act SDK property
+        XCTAssertEqualObjects(labels[@"navigation_level_1"], @"level 1");
+        XCTAssertEqualObjects(labels[@"navigation_level_2"], @"level 2");
+        XCTAssertEqualObjects(labels[@"navigation_level_3"], @"level 3");
+        XCTAssertEqualObjects(labels[@"navigation_level_4"], @"level 4");
+        XCTAssertEqualObjects(labels[@"navigation_level_5"], @"level 5");
+        XCTAssertEqualObjects(labels[@"navigation_level_6"], @"level 6");
+        XCTAssertEqualObjects(labels[@"navigation_level_7"], @"level 7");
+        XCTAssertEqualObjects(labels[@"navigation_level_8"], @"level 8");
+        XCTAssertNil(labels[@"navigation_level_9"]);
+        XCTAssertEqualObjects(labels[@"custom_label"], @"custom_value");
+        return YES;
+    }];
+    
+    NSArray<NSString *> *levels = @[ @"level 1", @"level 2", @"level 3", @"level 4", @"level 5", @"level 6", @"level 7", @"level 8", @"level 9" ];
+    
+    SRGAnalyticsPageViewLabels *labels = [[SRGAnalyticsPageViewLabels alloc] init];
+    labels.customInfo = @{ @"custom_label" : @"custom_value" };
+    
+    [SRGAnalyticsTracker.sharedTracker trackPageViewWithTitle:@"Page view" levels:levels labels:labels fromPushNotification:NO];
+    
+    [self waitForExpectationsWithTimeout:20. handler:nil];
+}
+
+- (void)testPageViewEventFromPushNotification
+{
+    [self expectationForPageViewEventNotificationWithHandler:^BOOL(NSString * event, NSDictionary * labels) {
+        XCTAssertEqualObjects(event, @"page_view"); // Commanders Act SDK property
+        XCTAssertEqualObjects(labels[@"page_type"], @"Page view"); // Commanders Act SDK property
+        XCTAssertNotNil(labels[@"accessed_after_push_notification"]);
+        XCTAssertTrue([labels[@"accessed_after_push_notification"] boolValue]);
+        return YES;
+    }];
+    
+    [SRGAnalyticsTracker.sharedTracker trackPageViewWithTitle:@"Page view" levels:nil labels:nil fromPushNotification:YES];
+    
+    [self waitForExpectationsWithTimeout:20. handler:nil];
+}
+
 @end
