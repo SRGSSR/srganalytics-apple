f81bba9ee2ad6feb44641175af6f740a45f57268
Pierre-Yves Bertholon <py.bertholon@gmail.com>
2023-07-16 13:50:18 +0200

    Bump to next major version

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 import PackageDescription
 
 struct ProjectSettings {
-    static let marketingVersion: String = "8.2.1"
+    static let marketingVersion: String = "9.0.0"
 }
 
 let package = Package(
