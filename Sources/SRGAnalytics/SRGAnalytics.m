d8a0a3dd0bb91d6ffd7879a45e76915d2e406705
Samuel Défago <defagos@gmail.com>
2024-03-18 13:52:04 +0100

    Add compilation warning about SRG Analytics decommissioning (#88)

--- Sources/SRGAnalytics/SRGAnalytics.m
+++ Sources/SRGAnalytics/SRGAnalytics.m
@@ -6,6 +6,8 @@
 
 #import "SRGAnalytics.h"
 
+#warning "SRG Analytics will be sunset in August 2025. Please upgrade to Pillarbox Analytics (https://github.com/SRGSSR/pillarbox-apple)."
+
 NSString *SRGAnalyticsMarketingVersion(void)
 {
     return @MARKETING_VERSION;
